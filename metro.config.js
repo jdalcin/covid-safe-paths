667ac1e081453d7ef825d2d1543e643e1f8b6942
Matt Buckley <matt@nicethings.io>
2020-05-25 12:37:26 -0400

    Exclude LocationService.js from bundle to bte target/flavor.

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,17 @@
  *
  * @format
  */
-
 const { getDefaultConfig } = require('metro-config');
 const defaultConfig = getDefaultConfig.getDefaultValues(__dirname);
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   resolver: {
     // react-native-local-resource
     assetExts: [...defaultConfig.resolver.assetExts, 'html'],
+    blacklistRE: blacklist([
+      `${process.env.TRACING_STRATEGY}` == 'bte' ? new RegExp(`app/services/LocationService.js`) : "",
+    ]),
   },
   transformer: {
     getTransformOptions: async () => ({
